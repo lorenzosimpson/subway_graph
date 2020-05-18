10528aed3f8132615bf87b7eaaaf87f44c83a3fd
Lorenzo Simpson <lorenzosimp@gmail.com>
2020-05-17 21:59:34 -0400

    removes whitespace

--- app.py
+++ app.py
@@ -17,9 +17,7 @@ def route():
     data = request.get_json()
     origin = data['origin'].lower()
     destination = data['destination'].lower()
-
     response = sg.route(origin, destination)
-
     return jsonify(response), 200
 
 
